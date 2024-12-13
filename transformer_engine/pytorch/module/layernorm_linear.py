Fix wgrad accumulation with delayed scaling
Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

transformer_engine/pytorch/module/layernorm_linear.py
@@ -313,6 +313,9 @@ class _LayerNormLinear(torch.autograd.Function):
             ctx.requires_dgrad = inp.requires_grad
             ctx.requires_wgrad = weight.requires_grad
 
+            if fuse_wgrad_accumulation:
+                ctx.main_grad = weight.main_grad
+
             ctx.grad_input_quantizer = grad_input_quantizer
             ctx.grad_output_quantizer = grad_output_quantizer
             ctx.input_quantizer = input_quantizer
@@ -383,7 +386,7 @@ class _LayerNormLinear(torch.autograd.Function):
             mu, rsigma = saved_tensors
 
             # Since main_grad can be modified inplace, it should not be a part of saved_tensors
-            main_grad = weight.main_grad if weight is not None and ctx.fuse_wgrad_accumulation and weight.requires_grad else None
+            main_grad = ctx.main_grad if weight is not None and ctx.fuse_wgrad_accumulation and ctx.requires_wgrad else None
 
             if ctx.grad_output_quantizer is not None:
                 ctx.grad_output_quantizer.set_usage(

transformer_engine/pytorch/module/layernorm_mlp.py
@@ -483,6 +483,9 @@ class _LayerNormMLP(torch.autograd.Function):
             ctx.fc1_weight = fc1_weight
             ctx.fc2_weight = fc2_weight
 
+            if fuse_wgrad_accumulation:
+                ctx.fc1_main_grad = fc1_weight.main_grad
+                ctx.fc2_main_grad = fc2_weight.main_grad
 
             ctx.device = device
             ctx.activation_dtype = activation_dtype
@@ -563,8 +566,8 @@ class _LayerNormMLP(torch.autograd.Function):
             fc1_weight_main_grad, fc2_weight_main_grad, mu, rsigma = saved_tensors
 
             # Since main_grad can be modified inplace, it should not be a part of saved_tensors
-            fc1_weight_main_grad = fc1_weight.main_grad if fc1_weight is not None and ctx.fuse_wgrad_accumulation and fc1_weight.requires_grad else None
-            fc2_weight_main_grad = fc2_weight.main_grad if fc2_weight is not None and ctx.fuse_wgrad_accumulation and fc2_weight.requires_grad else None
+            fc1_weight_main_grad = ctx.fc1_main_grad if fc1_weight is not None and ctx.fuse_wgrad_accumulation and ctx.fc1_weight_requires_grad else None
+            fc2_weight_main_grad = ctx.fc2_main_grad if fc2_weight is not None and ctx.fuse_wgrad_accumulation and ctx.fc2_weight_requires_grad else None
 
             # TODO: Fix this
             # Gather saved autograd context tensors when running with FSDP

transformer_engine/pytorch/module/linear.py
@@ -253,6 +253,10 @@ class _Linear(torch.autograd.Function):
             ctx.grad_output_quantizer = grad_output_quantizer
             ctx.grad_input_quantizer = grad_input_quantizer
             ctx.fuse_wgrad_accumulation = fuse_wgrad_accumulation
+
+            if fuse_wgrad_accumulation:
+                ctx.main_grad = weight.main_grad
+
             ctx.cpu_offloading = cpu_offloading
             ctx.is_first_microbatch = is_first_microbatch
             ctx.use_bias = bias is not None
@@ -298,7 +302,7 @@ class _Linear(torch.autograd.Function):
             ) = saved_tensors
 
             # Since main_grad can be modified inplace, it should not be a part of saved_tensors
-            main_grad = weight.main_grad if weight is not None and ctx.fuse_wgrad_accumulation and weight.requires_grad else None
+            main_grad = ctx.main_grad if weight is not None and ctx.fuse_wgrad_accumulation and ctx.requires_wgrad else None
 
             # Gather intermediate/activation tensors if needed
             # NOTE: weight_fp8 = weight when ctx.fp8 == False and torch.disttributed.FSDP already