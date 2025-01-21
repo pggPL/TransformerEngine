6b792be37396d1ed7be2afa93193b67e1dac0c79
Kirthi Shankar Sivamani <ksivamani@nvidia.com>
2025-01-21 22:09:19 +0000

    Format
    Signed-off-by: Kirthi Shankar Sivamani <ksivamani@nvidia.com>

--- transformer_engine/common/gemm/cublaslt_gemm.cu
+++ transformer_engine/common/gemm/cublaslt_gemm.cu
@@ -267,8 +267,9 @@ void cublas_gemm(const Tensor *inputA, const Tensor *inputB, Tensor *outputD,
       // Workaround for heuristic cache bug in cublasLt. This separates the MXFP8 cache key from non-block scaling.
       // CUBLASLT_MATMUL_DESC_ALPHA_VECTOR_BATCH_STRIDE is unused for block scaling so it's safe to set.
       const int64_t dummy_a_vec_stride = 1;
-      NVTE_CHECK_CUBLAS(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_ALPHA_VECTOR_BATCH_STRIDE,
-                                                       &dummy_a_vec_stride, sizeof(dummy_a_vec_stride)));
+      NVTE_CHECK_CUBLAS(cublasLtMatmulDescSetAttribute(
+          operationDesc, CUBLASLT_MATMUL_DESC_ALPHA_VECTOR_BATCH_STRIDE, &dummy_a_vec_stride,
+          sizeof(dummy_a_vec_stride)));
 #endif
     } else {
       NVTE_ERROR("Not implemented scaling modes: " + to_string(inputA->scaling_mode) + " and  " +
